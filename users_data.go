8af02ff1df7e5b8753e683e1e4557651f258df1d
Aditya Jha <adityajha@MacBook-Pro.local>
2023-10-29 22:26:07 +0530

    Added sample user records to mock the db

---
+++ users_data.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/thisisadi/Total-Corp-Task/user"
+)
+
+var Users = []*user.User{
+	{Id: 1, Fname: "Steve", City: "LA", Phone: 1234567890, Height: 5.8, Married: true},
+	{Id: 2, Fname: "John", City: "NY", Phone: 9876543210, Height: 6.0, Married: false},
+	{Id: 3, Fname: "Jane", City: "SF", Phone: 5555555555, Height: 5.5, Married: false},
+	{Id: 4, Fname: "Anna", City: "LA", Phone: 9998887777, Height: 5.7, Married: true},
+	{Id: 5, Fname: "Michael", City: "SF", Phone: 4443332222, Height: 5.9, Married: true},
+	{Id: 6, Fname: "Emily", City: "NY", Phone: 1112223333, Height: 5.6, Married: false},
+	{Id: 7, Fname: "David", City: "LA", Phone: 7778889999, Height: 5.10, Married: false},
+	{Id: 8, Fname: "Sara", City: "NY", Phone: 3332221111, Height: 5.4, Married: true},
+	{Id: 9, Fname: "Matthew", City: "SF", Phone: 6667778888, Height: 5.11, Married: true},
+	{Id: 10, Fname: "Olivia", City: "LA", Phone: 2223334444, Height: 5.8, Married: false},
+	{Id: 11, Fname: "Daniel", City: "NY", Phone: 8889990000, Height: 6.1, Married: false},
+	{Id: 12, Fname: "Ava", City: "SF", Phone: 4445556666, Height: 5.5, Married: true},
+	{Id: 13, Fname: "Ethan", City: "LA", Phone: 5556667777, Height: 6.2, Married: false},
+	{Id: 14, Fname: "Mia", City: "NY", Phone: 6667778888, Height: 5.6, Married: true},
+	{Id: 15, Fname: "Liam", City: "SF", Phone: 7778889999, Height: 5.9, Married: true},
+	{Id: 16, Fname: "Isabella", City: "LA", Phone: 8889990000, Height: 5.7, Married: false},
+	{Id: 17, Fname: "Aiden", City: "NY", Phone: 9998887777, Height: 6.0, Married: false},
+	{Id: 18, Fname: "Sophia", City: "SF", Phone: 1234567890, Height: 5.5, Married: true},
+	{Id: 19, Fname: "Mason", City: "LA", Phone: 9876543210, Height: 5.8, Married: false},
+	{Id: 20, Fname: "Charlotte", City: "NY", Phone: 5555555555, Height: 6.1, Married: true},
+	{Id: 21, Fname: "Logan", City: "SF", Phone: 1112223333, Height: 5.6, Married: false},
+	{Id: 22, Fname: "Amelia", City: "LA", Phone: 4443332222, Height: 5.9, Married: true},
+	{Id: 23, Fname: "Lucas", City: "NY", Phone: 9990001111, Height: 5.7, Married: false},
+	{Id: 24, Fname: "Harper", City: "SF", Phone: 6667778888, Height: 5.4, Married: false},
+	{Id: 25, Fname: "Elijah", City: "LA", Phone: 2223334444, Height: 6.2, Married: true},
+	{Id: 26, Fname: "Grace", City: "NY", Phone: 7778889999, Height: 5.8, Married: false},
+	{Id: 27, Fname: "Landon", City: "SF", Phone: 3332221111, Height: 5.11, Married: true},
+	{Id: 28, Fname: "Lily", City: "LA", Phone: 8889990000, Height: 5.5, Married: false},
+	{Id: 29, Fname: "Nathan", City: "NY", Phone: 5556667777, Height: 6.0, Married: true},
+}
